194fca0ade00f6df90fda826a4d3b085a330fad6
Rajshree Kale <rajshree.kale@merkle.com>
2025-05-12 17:26:43 +0530

    Content added for blog page

--- app/components/BlogList/BlogList.tsx
+++ app/components/BlogList/BlogList.tsx
@@ -2,16 +2,22 @@ import { useEffect, useState } from 'react';
 import type { BlogPost } from '~/types/contefultypes';
 import './BlogList.css';
 
-export function BlogList({ posts }: { posts: BlogPost[] }) {
+export function BlogList({
+  posts,
+  blogListItems,
+}: {
+  posts: BlogPost[];
+  blogListItems: any[];
+}) {
   const [expandedIndex, setExpandedIndex] = useState(0);
-
+  console.log("Blog List Items in browser:", blogListItems);
   useEffect(() => {
     console.log(posts);
   }, [posts]);
 
   return (
     <section className="blog-list-container">
-      <h1 className="blog-title">Latest Blog</h1>
+      {/* <h1 className="blog-title">Latest Blog</h1> */}
       <div className="blog-list-grid">
         {posts.map((post, index) => (
           <article
@@ -31,6 +37,30 @@ export function BlogList({ posts }: { posts: BlogPost[] }) {
           </article>
         ))}
       </div>
+      {blogListItems?.map((item: any, index: number) => (
+        <div key={index} className="blog-featured-section">
+         
+          <h2 className="featured-heading">{item.heading}</h2>
+
+         
+          {item.list?.map((listItem: any, subIndex: number) => (
+            <div
+              key={`${index}-${subIndex}`}
+              className={`blog-featured-item ${
+                subIndex % 2 === 0 ? 'row-normal' : 'row-reverse'
+              }`}
+            >
+              <div className="featured-image">
+                <img src={listItem.imageUrl} alt={listItem.title} />
+              </div>
+              <div className="featured-content">
+                <h3>{listItem.title}</h3>
+                <p>{listItem.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
     </section>
   );
 }
